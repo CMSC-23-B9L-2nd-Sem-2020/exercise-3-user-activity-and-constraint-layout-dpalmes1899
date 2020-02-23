745c0f1e04ffe391827b9b7584df4400d80f690b
dpalmes1899 <ddpalmes2@up.edu.ph>
2020-02-23 12:55:51 +0800

    feat:finished implementation of lights out game

--- app/src/main/java/com/android/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/android/example/colormyviews/MainActivity.kt
@@ -16,9 +16,14 @@
 
 package com.android.example.colormyviews
 
+import android.content.Context
 import android.graphics.Color
 import android.os.Bundle
+import android.text.Layout
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -28,65 +33,348 @@ import androidx.appcompat.app.AppCompatActivity
  */
 class MainActivity : AppCompatActivity() {
 
+    var count = 0
+    var text = "Clicks: " + count
+    lateinit var countNumber:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        countNumber =  findViewById(R.id.click_count)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            changeNickname(it)
+        }
+
+        findViewById<TextView>(R.id.nickname_text).setOnClickListener {
+            updateNickname(it)
+        }
+
         setListeners()
+
     }
 
     /**
      * Attaches listeners to all the views.
      */
-    private fun setListeners() {
 
-        val boxOneText = findViewById<TextView>(R.id.box_one_text)
-        val boxTwoText = findViewById<TextView>(R.id.box1)
-        val boxThreeText = findViewById<TextView>(R.id.box_three_text)
-        val boxFourText = findViewById<TextView>(R.id.box_four_text)
-        val boxFiveText = findViewById<TextView>(R.id.box_five_text)
+    private fun setListeners(){
+        val boxText = findViewById<TextView>(R.id.box)
+        val box1Text = findViewById<TextView>(R.id.box1)
+        val box2Text = findViewById<TextView>(R.id.box2)
+        val box3Text = findViewById<TextView>(R.id.box3)
+        val box4Text = findViewById<TextView>(R.id.box4)
+        val box5Text = findViewById<TextView>(R.id.box5)
+        val box6Text = findViewById<TextView>(R.id.box6)
+        val box7Text = findViewById<TextView>(R.id.box7)
+        val box8Text = findViewById<TextView>(R.id.box8)
+        val box9Text = findViewById<TextView>(R.id.box9)
+        val box10Text = findViewById<TextView>(R.id.box10)
+        val box11Text = findViewById<TextView>(R.id.box11)
+        val box12Text = findViewById<TextView>(R.id.box12)
+        val box13Text = findViewById<TextView>(R.id.box13)
+        val box14Text = findViewById<TextView>(R.id.box14)
+        val box15Text = findViewById<TextView>(R.id.box15)
+        val box16Text = findViewById<TextView>(R.id.box16)
+        val box17Text = findViewById<TextView>(R.id.box17)
+        val box18Text = findViewById<TextView>(R.id.box18)
+        val box19Text = findViewById<TextView>(R.id.box19)
+        val box20Text = findViewById<TextView>(R.id.box20)
+        val box21Text = findViewById<TextView>(R.id.box21)
+        val box22Text = findViewById<TextView>(R.id.box22)
+        val box23Text = findViewById<TextView>(R.id.box23)
+        val box24Text = findViewById<TextView>(R.id.box24)
 
-        val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
+        val retryButton = findViewById<TextView>(R.id.retry_button)
 
-        val redButton = findViewById<TextView>(R.id.red_button)
-        val greenButton = findViewById<TextView>(R.id.green_button)
-        val yellowButton = findViewById<TextView>(R.id.yellow_button)
+        val twoDimensional: List<List<View>> = listOf(
+            listOf(boxText, box1Text, box2Text, box3Text, box4Text),
+            listOf(box5Text, box6Text, box7Text, box8Text, box9Text),
+            listOf(box10Text, box11Text, box12Text, box13Text, box14Text),
+            listOf(box15Text, box16Text, box17Text, box18Text, box19Text),
+            listOf(box20Text, box21Text, box22Text, box23Text, box24Text)
+        )
 
-        val clickableViews: List<View> =
-            listOf(
-                boxOneText, boxTwoText, boxThreeText,
-                boxFourText, boxFiveText, rootConstraintLayout,
-                redButton, greenButton, yellowButton
-            )
+        var indicator: Array<Array<Int>> = arrayOf<Array<Int>>()
+        for (i in 0..4) {
+            var array = arrayOf<Int>()
+            for (j in 0..4) {
+                array += 0
+            }
+            indicator += array
+        }
+        retryButton.setOnClickListener{retry(twoDimensional,indicator)}
 
-        for (item in clickableViews) {
-            item.setOnClickListener { makeColored(it) }
+        for(item: Int in (0..4)){
+            for (item2: Int in(0..4)){
+                twoDimensional[item][item2].setOnClickListener {flipLights(it,twoDimensional,item,item2,indicator)}
+            }
         }
     }
 
-    /**
-     * Sets the background color of a view depending on it's resource id.
-     * This is a way of using one click handler to do similar operations on a
-     * group of views.
-     */
+    private fun flipLights(view: View,twodimensional : List<List<View>>,row:Int,col:Int,indicator:Array<Array<Int>> ){
+            count ++
+            text = "Clicks: " + count
+            countNumber.setText(text)
+           if(indicator[row][col] == 0) {
+               view.setBackgroundColor(Color.BLACK)
+               indicator[row][col] = 1
+           }else{
+               view.setBackgroundColor(Color.WHITE)
+               indicator[row][col] = 0
+           }
 
-    private fun makeColored(view: View) {
-        when (view.id) {
+        if(row == 0) when (col){  //top
+            0 -> { //left most
+                if(indicator[row][col+1]==0) { //right white
+                    twodimensional[row][col+1].setBackgroundColor(Color.BLACK)
+                    indicator[row][col+1] = 1
 
-            // Boxes using Color class colors for background
-            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
-            R.id.box1 -> view.setBackgroundColor(Color.GRAY)
-
-            R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
-            R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
-            R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
-
-            // Boxes using custom colors for background
-            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
-            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
-            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
-
-            else -> view.setBackgroundColor(Color.LTGRAY)
+                }else{ //right black
+                    twodimensional[row][col+1].setBackgroundColor(Color.WHITE)
+                    indicator[row][col+1] = 0
+                }
+                if(indicator[row+1][col]==0){ //down white
+                    twodimensional[row+1][col].setBackgroundColor(Color.BLACK)
+                    indicator[row+1][col] = 1
+                }else{ //down black
+                    twodimensional[row+1][col].setBackgroundColor(Color.WHITE)
+                    indicator[row+1][col] = 0
+                }
+            }
+            4 -> { //rightmost
+                if(indicator[row][col-1]==0) { //left white
+                    twodimensional[row][col-1].setBackgroundColor(Color.BLACK)
+                    indicator[row][col-1] = 1
+                }else{
+                    twodimensional[row][col-1].setBackgroundColor(Color.WHITE)
+                    indicator[row][col-1] = 0
+                }
+                if(indicator[row+1][col]==0){ //down white
+                    twodimensional[row+1][col].setBackgroundColor(Color.BLACK)
+                    indicator[row+1][col] = 1
+                }else{
+                    twodimensional[row+1][col].setBackgroundColor(Color.WHITE)
+                    indicator[row+1][col] = 0
+                }
+            }
+            else -> {
+                if(indicator[row][col+1]==0) { //right white
+                    twodimensional[row][col+1].setBackgroundColor(Color.BLACK)
+                    indicator[row][col+1] = 1
+                }else{
+                    twodimensional[row][col+1].setBackgroundColor(Color.WHITE)
+                    indicator[row][col+1] = 0
+                }
+                if(indicator[row][col-1]==0) { //left white
+                    twodimensional[row][col-1].setBackgroundColor(Color.BLACK)
+                    indicator[row][col-1] = 1
+                }else{
+                    twodimensional[row][col-1].setBackgroundColor(Color.WHITE)
+                    indicator[row][col-1] = 0
+                }
+                if(indicator[row+1][col]==0){ //down white
+                    twodimensional[row+1][col].setBackgroundColor(Color.BLACK)
+                    indicator[row+1][col] = 1
+                }else{
+                    twodimensional[row+1][col].setBackgroundColor(Color.WHITE)
+                    indicator[row+1][col] = 0
+                }
+            }
         }
+
+        else if(row == 4) when (col){ //bottom
+            0 -> { //left most
+                if(indicator[row][col+1]==0) { //right white
+                    twodimensional[row][col+1].setBackgroundColor(Color.BLACK)
+                    indicator[row][col+1] = 1
+
+                }else{ //right black
+                    twodimensional[row][col+1].setBackgroundColor(Color.WHITE)
+                    indicator[row][col+1] = 0
+                }
+                if(indicator[row-1][col]==0){ //up white
+                    twodimensional[row+-1][col].setBackgroundColor(Color.BLACK)
+                    indicator[row-1][col] = 1
+                }else{ //up black
+                    twodimensional[row-1][col].setBackgroundColor(Color.WHITE)
+                    indicator[row-1][col] = 0
+                }
+            }
+            4 -> { //rightmost
+                if(indicator[row][col-1]==0) { //left white
+                    twodimensional[row][col-1].setBackgroundColor(Color.BLACK)
+                    indicator[row][col-1] = 1
+                }else{
+                    twodimensional[row][col-1].setBackgroundColor(Color.WHITE)
+                    indicator[row][col-1] = 0
+                }
+                if(indicator[row-1][col]==0){ //up white
+                    twodimensional[row-1][col].setBackgroundColor(Color.BLACK)
+                    indicator[row-1][col] = 1
+                }else{ //up black
+                    twodimensional[row-1][col].setBackgroundColor(Color.WHITE)
+                    indicator[row-1][col] = 0
+                }
+            }
+            else -> {
+                if(indicator[row][col+1]==0) { //right white
+                    twodimensional[row][col+1].setBackgroundColor(Color.BLACK)
+                    indicator[row][col+1] = 1
+                }else{
+                    twodimensional[row][col+1].setBackgroundColor(Color.WHITE)
+                    indicator[row][col+1] = 0
+                }
+                if(indicator[row][col-1]==0) { //left white
+                    twodimensional[row][col-1].setBackgroundColor(Color.BLACK)
+                    indicator[row][col-1] = 1
+                }else{
+                    twodimensional[row][col-1].setBackgroundColor(Color.WHITE)
+                    indicator[row][col-1] = 0
+                }
+                if(indicator[row-1][col]==0){ //up white
+                    twodimensional[row-1][col].setBackgroundColor(Color.BLACK)
+                    indicator[row-1][col] = 1
+                }else{
+                    twodimensional[row-1][col].setBackgroundColor(Color.WHITE)
+                    indicator[row-1][col] = 0
+                }
+            }
+        }
+
+        if(col == 0) {  // left vertical
+            if(row in 1..3) {
+                if(indicator[row-1][col]==0){ //up white
+                    twodimensional[row-1][col].setBackgroundColor(Color.BLACK)
+                    indicator[row-1][col] = 1
+                }else{ //up black
+                    twodimensional[row-1][col].setBackgroundColor(Color.WHITE)
+                    indicator[row-1][col] = 0
+                }
+
+                if(indicator[row+1][col]==0){ //down white
+                    twodimensional[row+1][col].setBackgroundColor(Color.BLACK)
+                    indicator[row+1][col] = 1
+                }else{
+                    twodimensional[row+1][col].setBackgroundColor(Color.WHITE)
+                    indicator[row+1][col] = 0
+                }
+
+                if(indicator[row][col+1]==0) { //right white
+                    twodimensional[row][col+1].setBackgroundColor(Color.BLACK)
+                    indicator[row][col+1] = 1
+
+                }else{ //right black
+                    twodimensional[row][col+1].setBackgroundColor(Color.WHITE)
+                    indicator[row][col+1] = 0
+                }
+
+            }
+        }
+        else if(col == 4){
+            if(row in 1..3){
+                if(indicator[row-1][col]==0){ //up white
+                    twodimensional[row-1][col].setBackgroundColor(Color.BLACK)
+                    indicator[row-1][col] = 1
+                }else{ //up black
+                    twodimensional[row-1][col].setBackgroundColor(Color.WHITE)
+                    indicator[row-1][col] = 0
+                }
+
+                if(indicator[row+1][col]==0){ //down white
+                    twodimensional[row+1][col].setBackgroundColor(Color.BLACK)
+                    indicator[row+1][col] = 1
+                }else{
+                    twodimensional[row+1][col].setBackgroundColor(Color.WHITE)
+                    indicator[row+1][col] = 0
+                }
+
+                if(indicator[row][col-1]==0) { //left white
+                    twodimensional[row][col-1].setBackgroundColor(Color.BLACK)
+                    indicator[row][col-1] = 1
+                }else{
+                    twodimensional[row][col-1].setBackgroundColor(Color.WHITE)
+                    indicator[row][col-1] = 0
+                }
+            }
+        }
+        if(row in 1..3 && col in 1..3){ //middle
+            if(indicator[row-1][col]==0){ //up white
+                twodimensional[row-1][col].setBackgroundColor(Color.BLACK)
+                indicator[row-1][col] = 1
+            }else{ //up black
+                twodimensional[row-1][col].setBackgroundColor(Color.WHITE)
+                indicator[row-1][col] = 0
+            }
+
+            if(indicator[row+1][col]==0){ //down white
+                twodimensional[row+1][col].setBackgroundColor(Color.BLACK)
+                indicator[row+1][col] = 1
+            }else{
+                twodimensional[row+1][col].setBackgroundColor(Color.WHITE)
+                indicator[row+1][col] = 0
+            }
+
+            if(indicator[row][col-1]==0) { //left white
+                twodimensional[row][col-1].setBackgroundColor(Color.BLACK)
+                indicator[row][col-1] = 1
+            }else{
+                twodimensional[row][col-1].setBackgroundColor(Color.WHITE)
+                indicator[row][col-1] = 0
+            }
+
+            if(indicator[row][col+1]==0) { //right white
+                twodimensional[row][col+1].setBackgroundColor(Color.BLACK)
+                indicator[row][col+1] = 1
+
+            }else{ //right black
+                twodimensional[row][col+1].setBackgroundColor(Color.WHITE)
+                indicator[row][col+1] = 0
+            }
+        }
+    }
+
+    private fun retry(twoDimensional : List<List<View>>,indicator:Array<Array<Int>>){
+
+        for(item: Int in (0..4)){
+            for (item2: Int in(0..4)){
+                twoDimensional[item][item2].setBackgroundColor(Color.WHITE)
+            }
+        }
+
+        for (i in 0..4) {
+            for (j in 0..4) {
+                indicator[i][j] = 0
+            }
+        }
+
+        count = 0
+    }
+
+    private fun changeNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        nicknameTextView.text = editText.text
+
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameTextView.visibility = View.VISIBLE
+
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+
+    private fun updateNickname (view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val doneButton = findViewById<Button>(R.id.done_button)
+
+        editText.visibility = View.VISIBLE
+        doneButton.visibility = View.VISIBLE
+        view.visibility = View.GONE
+
+        editText.requestFocus()
+
     }
 }
+
